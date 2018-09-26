344f22db8234307ff65f0d505c952123360217a5
Michael Tintiuc <contact@michaeltintiuc.com>
2018-09-26 23:45:19 +0300

    GEN-29

--- src/BBitmap.cpp
+++ src/BBitmap.cpp
@@ -318,10 +318,314 @@ void BBitmap::Clear(TUint8 aColor) {
   memset(mPixels, aColor, mPitch * mHeight);
 }
 
-void BBitmap::DrawLine(TUint8 aColor, TInt aX1, TInt aY1, TInt aX2, TInt aY2) {
-  // TODO: Jay implement this
+void BBitmap::DrawFastHLine(BViewPort *aViewPort, TInt aX, TInt aY, TUint aW, TUint8 aColor) {
+  // Initial viewport offset
+  TInt viewPortOffsetX = 0;
+  TInt viewPortOffsetY = 0;
+
+  // Create the viewport
+  TRect clipRect;
+  if (aViewPort) {
+    aViewPort->GetRect(clipRect);
+    viewPortOffsetX = aViewPort->mOffsetX;
+    viewPortOffsetY = aViewPort->mOffsetY;
+  } else {
+    clipRect.Set(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  }
+
+  // Store viewport width/height
+  const TInt clipRectWidth = clipRect.Width();
+  const TInt clipRectHeight = clipRect.Height();
+
+  // Adjust for viewport offset
+  aX += viewPortOffsetX;
+  aY += viewPortOffsetY;
+
+  // Do y bounds checks
+  if (aY < viewPortOffsetY || aY >= clipRectHeight) {
+    return;
+  }
+
+  // last x point + 1
+  TInt16 xEnd = aX + aW;
+
+  // Check if the entire line is not on the display
+  if (xEnd <= viewPortOffsetX || aX >= clipRectWidth) {
+    return;
+  }
+
+  // Don't start before the left edge
+  if (aX < viewPortOffsetX) {
+    aX = viewPortOffsetX;
+  }
+
+  // Don't end past the right edge
+  if (xEnd > clipRectWidth) {
+    xEnd = clipRectWidth;
+  }
+
+  // calculate actual width (even if unchanged)
+  aW = xEnd - aX;
+
+  while (aW--) {
+    WritePixel(aX++, aY, aColor);
+  }
 }
 
-void BBitmap::DrawRect(TUint8 aColor, TInt aX1, TInt aY1, TInt aX2, TInt aY2) {
-  // TODO: Jay implement this
+void BBitmap::DrawFastVLine(BViewPort *aViewPort, TInt aX, TInt aY, TUint aH, TUint8 aColor) {
+  // Initial viewport offset
+  TInt viewPortOffsetX = 0;
+  TInt viewPortOffsetY = 0;
+
+  // Create the viewport
+  TRect clipRect;
+  if (aViewPort) {
+    aViewPort->GetRect(clipRect);
+    viewPortOffsetX = aViewPort->mOffsetX;
+    viewPortOffsetY = aViewPort->mOffsetY;
+  } else {
+    clipRect.Set(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  }
+
+  // Store viewport width/height
+  const TInt clipRectWidth = clipRect.Width();
+  const TInt clipRectHeight = clipRect.Height();
+
+  // Adjust for viewport offset
+  aX += viewPortOffsetX;
+  aY += viewPortOffsetY;
+
+  // Do x bounds checks
+  if (aX < viewPortOffsetX || aX >= clipRectWidth) {
+    return;
+  }
+
+  // last y point + 1
+  TInt16 yEnd = aY + aH;
+
+  // Check if the entire line is not on the display
+  if (yEnd <= viewPortOffsetY || aY >= clipRectHeight) {
+    return;
+  }
+
+  // Don't start before the top edge
+  if (aY < viewPortOffsetY) {
+    aY = viewPortOffsetY;
+  }
+
+  // Don't end past the bottom edge
+  if (yEnd > clipRectHeight) {
+    yEnd = clipRectWidth;
+  }
+
+  // calculate actual height (even if unchanged)
+  aH = yEnd - aY;
+
+  while (aH--) {
+    WritePixel(aX, aY++, aColor);
+  }
+}
+
+void BBitmap::DrawLine(BViewPort *aViewPort, TInt aX1, TInt aY1, TInt aX2, TInt aY2, TUint8 aColor) {
+  TInt viewPortOffsetX = 0;
+  TInt viewPortOffsetY = 0;
+
+  // Create the viewport
+  TRect clipRect;
+  if (aViewPort) {
+    aViewPort->GetRect(clipRect);
+    viewPortOffsetX = aViewPort->mOffsetX;
+    viewPortOffsetY = aViewPort->mOffsetY;
+  } else {
+    clipRect.Set(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  }
+
+  // Store viewport width/height
+  const TInt clipRectWidth = clipRect.Width();
+  const TInt clipRectHeight = clipRect.Height();
+
+  // Adjust for viewport offset
+  aX1 += viewPortOffsetX;
+  aX2 += viewPortOffsetX;
+  aY1 += viewPortOffsetY;
+  aY2 += viewPortOffsetY;
+
+  // Draw simple lines if possible
+  if (aY1 == aY2) {
+    if (aX2 > aX1) {
+      DrawFastHLine(aViewPort, aX1, aY1, aX2 - aX1 + 1, aColor);
+    } else if (aX2 < aX1) {
+      DrawFastHLine(aViewPort, aX2, aY1, aX1 - aX2 + 1, aColor);
+    } else {
+      WritePixel(aX1, aY1, aColor);
+    }
+    return;
+  } else if (aX1 == aX2) {
+    if (aY2 > aY1) {
+      DrawFastVLine(aViewPort, aX1, aY1, aY2 - aY1 + 1, aColor);
+    } else {
+      DrawFastVLine(aViewPort, aX1, aY2, aY1 - aY2 + 1, aColor);
+    }
+    return;
+  }
+
+  // Bresenham's algorithm
+  // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+  const TBool steep = ABS(aY2 - aY1) > ABS(aX2 - aX1);
+
+  if (steep) {
+    TInt16 temp = aX1;
+    aX1 = aY1;
+    aY1 = temp;
+
+    temp = aX2;
+    aX2 = aY2;
+    aY2 = temp;
+  }
+
+  if (aX1 > aX2) {
+    TInt16 temp = aX1;
+    aX1 = aX2;
+    aX2 = temp;
+
+    temp = aY1;
+    aY1 = aY2;
+    aY2 = temp;
+  }
+
+  const TInt16 dx = aX2 - aX1;
+  const TInt16 dy = ABS(aY2 - aY1);
+  const TInt8 ystep = aY1 < aY2 ? 1 : -1;
+
+  TInt16 err = dx / 2;
+
+  for (; aX1 <= aX2; aX1++) {
+    if (steep) {
+      // aY1 is X coord and aX1 is Y coord in this case
+      if ((aY1 >= viewPortOffsetX) && (aY1 <= clipRectWidth) && (aX1 >= viewPortOffsetY) && (aX1 < clipRectHeight)) {
+        WritePixel(aY1, aX1, aColor);
+      }
+    } else {
+      // aX1 is X coord and aY1 is Y coord in this case
+      if ((aX1 >= viewPortOffsetX) && (aX1 <= clipRectWidth) && (aY1 >= viewPortOffsetY) && (aY1 < clipRectHeight)) {
+        WritePixel(aX1, aY1, aColor);
+      }
+    }
+
+    err -= dy;
+
+    if (err < 0) {
+      aY1 += ystep;
+      err += dx;
+    }
+  }
+}
+
+void BBitmap::DrawRect(BViewPort *aViewPort, TInt aX1, TInt aY1, TInt aX2, TInt aY2, TUint8 aColor) {
+  DrawFastHLine(aViewPort, aX1, aY1, aX2, aColor);
+  DrawFastHLine(aViewPort, aX1, aY1 + aY2 - 1, aX2, aColor);
+  DrawFastVLine(aViewPort, aX1, aY1, aY2, aColor);
+  DrawFastVLine(aViewPort, aX1 + aX2 - 1, aY1, aY2, aColor);
+}
+
+void BBitmap::FillRect(BViewPort *aViewPort, TInt aX1, TInt aY1, TInt aX2, TInt aY2, TUint8 aColor) {
+  const TInt w = aX1 + aX2;
+  for (; aX1 < w; aX1++) {
+    DrawFastVLine(aViewPort, aX1, aY1, aY2, aColor);
+  }
+}
+
+void BBitmap::DrawCircle(BViewPort *aViewPort, TInt aX, TInt aY, TUint r, TUint8 aColor) {
+  TInt viewPortOffsetX = 0;
+  TInt viewPortOffsetY = 0;
+
+  // Create the viewport
+  TRect clipRect;
+  if (aViewPort) {
+    aViewPort->GetRect(clipRect);
+    viewPortOffsetX = aViewPort->mOffsetX;
+    viewPortOffsetY = aViewPort->mOffsetY;
+  } else {
+    clipRect.Set(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  }
+
+  aX += viewPortOffsetX;
+  aY += viewPortOffsetY;
+  TInt f = 1 - r;
+  TInt ddF_x = 1;
+  TInt ddF_y = -2 * r;
+  TInt x = 0;
+  TInt y = r;
+  TInt xx1, xx2, xx3, xx4, yy1, yy2, yy3, yy4;
+
+  SafeWritePixel(aX, aY + r, aColor);
+  SafeWritePixel(aX, aY - r, aColor);
+  SafeWritePixel(aX + r, aY, aColor);
+  SafeWritePixel(aX - r, aY, aColor);
+
+  while (x < y) {
+    if (f >= 0) {
+      y--;
+      ddF_y += 2;
+      f += ddF_y;
+    }
+
+    x++;
+    ddF_x += 2;
+    f += ddF_x;
+
+    xx1 = aX + x;
+    xx2 = aX - x;
+    xx3 = aX + y;
+    xx4 = aX - y;
+    yy1 = aY + y;
+    yy2 = aY - y;
+    yy3 = aY + x;
+    yy4 = aY - x;
+
+    SafeWritePixel(xx1, yy1, aColor);
+    SafeWritePixel(xx2, yy1, aColor);
+    SafeWritePixel(xx1, yy2, aColor);
+    SafeWritePixel(xx2, yy2, aColor);
+    SafeWritePixel(xx3, yy3, aColor);
+    SafeWritePixel(xx4, yy3, aColor);
+    SafeWritePixel(xx3, yy4, aColor);
+    SafeWritePixel(xx4, yy4, aColor);
+  }
+}
+
+void BBitmap::FillCircle(BViewPort *aViewPort, TInt aX, TInt aY, TUint r, TUint8 aColor) {
+  DrawFastVLine(aViewPort, aX, aY - r, 2 * r + 1, aColor);
+  FillCircleHelper(aViewPort, aX, aY, r, 3, 0, aColor);
+}
+
+void BBitmap::FillCircleHelper(BViewPort *aViewPort, TInt aX, TInt aY, TUint r, TUint sides, TUint delta, TUint8 aColor) {
+  TInt f = 1 - r;
+  TInt ddF_x = 1;
+  TInt ddF_y = -2 * r;
+  TInt x = 0;
+  TInt y = r;
+
+  while (x < y) {
+    if (f >= 0) {
+      y--;
+      ddF_y += 2;
+      f += ddF_y;
+    }
+
+    x++;
+    ddF_x += 2;
+    f += ddF_x;
+
+    // right side
+    if (sides & 0x1) {
+      DrawFastVLine(aViewPort, aX + x, aY - y, 2 * y + 1 + delta, aColor);
+      DrawFastVLine(aViewPort, aX + y, aY - x, 2 * x + 1 + delta, aColor);
+    }
+
+    if (sides & 0x2) { // left side
+      DrawFastVLine(aViewPort, aX - x, aY - y, 2 * y + 1 + delta, aColor);
+      DrawFastVLine(aViewPort, aX - y, aY - x, 2 * x + 1 + delta, aColor);
+    }
+  }
 }
